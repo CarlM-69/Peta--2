570350b969349532db1ef1e1f1113483827ef110
Carl <carlmathewgabay123@gmail.com>
2024-05-07 03:14:29 +0800

    maj-c

--- Scripts/Universal.js
+++ Scripts/Universal.js
@@ -1,1 +1,11 @@
-const username_display = document.querySelector("username_change")
+// Variables
+const username_display = document.querySelector("username_change");
+const xhr = new XMLHttpRequest();
+
+// Functions
+function sha256(pass) {
+    return CryptoJS.SHA256(pass).toString(CryptoJS.enc.Hex);
+}
+
+
+// Magic
