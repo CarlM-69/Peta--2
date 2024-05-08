b0e57a1172fdf0282be3605626415f7494915d7c
Carl <carlmathewgabay123@gmail.com>
2024-05-09 00:15:01 +0800

    cm

--- Scripts/User/Login.js
+++ Scripts/User/Login.js
@@ -1,19 +1,43 @@
+const login_background = document.querySelector("#login-bg");
+const container = document.querySelector("#container");
+const login_form = document.querySelector("#login-form");
+const login_username_input = document.querySelector("#username-input");
+const login_password_input = document.querySelector("#password-input");
+const eye_show_password_btn = document.querySelector("#eye");
+const notification_container = document.querySelector("#notif-container");
+
 // Functions
 function sha256(pass) {
 	// It changes the "pass" to encrypted "pass" for security
 	return CryptoJS.SHA256(pass).toString(CryptoJS.enc.Hex);
 }
 
+function createNotif(title, message) {
+	let newNotif = document.createElement("div");
+	newNotif.classList.add("notif-card");
+
+	console.log(title, message);
+
+	newNotif.innerHTML = `
+	<img src="../../Resources/Icons/exclamation.svg">
+	<div class="text_content">
+		<span>${ title }</span>
+		<span>${ message }</span>
+	</div>
+	<div class="exit">
+		<img src="../../Resources/Icons/xmark.svg">	
+	</div>
+	`;
+	notification_container.appendChild(newNotif);
+
+	setTimeout(function() {
+		newNotif.remove();
+	}, 5000);
+}
+
 
 // Magic
 window.addEventListener("DOMContentLoaded", function() {
-	const login_background = document.querySelector("#login-bg");
-	const container = document.querySelector("#container");
-	const login_form = document.querySelector("#login-form");
-	const login_username_input = document.querySelector("#username-input");
-	const login_password_input = document.querySelector("#password-input");
-	const login_form_btn = document.querySelector("#submit-btn");
-	const eye_show_password_btn = document.querySelector("#eye");
 	let pass_shown = false;
 
 	setTimeout(function() {
@@ -47,7 +71,10 @@ window.addEventListener("DOMContentLoaded", function() {
 		event.preventDefault();
 
 		if(login_username_input.value.length <= 0) {
-
+			createNotif(
+				"Username not Found!",
+				"Make sure that you typed your username correctly."
+			);
 		}
 
 		if(login_password_input.value.length <= 0) {
