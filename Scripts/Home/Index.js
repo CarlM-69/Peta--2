b7ca9e80d9554b9ae48ac54798ad35242aa980ed
Carl <carlmathewgabay123@gmail.com>
2024-05-12 15:44:00 +0800

    m-c

--- Scripts/Home/Index.js
+++ Scripts/Home/Index.js
@@ -1,42 +1,55 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var r = document.querySelectorAll("input[name='r']");
-    let time = Date.now();
-    let lastScrollUpdate = time;
-    let next = 1;
-    let reversing = false;
+	var r = document.querySelectorAll("input[name='r']");
+	let time = Date.now();
+	let lastScrollUpdate = time;
+	let next = 1;
+	let reversing = false;
 
-    function doTask() {
-        if((Date.now() - time) >= 7000) {
-            lastScrollUpdate = time;
-            time = Date.now();
-            r[next].checked = true;
+	r.forEach(function(i, idx) {
+		r[idx].addEventListener("click", function() {
+			lastScrollUpdate = time;
+			time = Date.now();
 
-            if(reversing == false) {
-                if(next == 4) {
-                    next = 3;
-                    reversing = true;
-                }
-                else {
-                    next += 1;
-                }
-            }
-            else {
-                if(next == 0) {
-                    next = 1;
-                    reversing = false;
-                }
-                else {
-                    next -= 1;
-                }
-            }
-        }
-        
-        if((Date.now() - lastScrollUpdate) >= 500) {
-            lastScrollUpdate = Date.now()
-        }
+			if(idx == 4) {
+				next = 3;
+				reversing = true;
+			}
+			else if(idx == 0) {
+				next = 1;
+				reversing = false;
+			}
+			else {
+				if(reversing) next = idx - 1;
+				else next = idx + 1;
+			}
+		});
+	});
 
-        setTimeout(doTask, 500);
-    }
+	function doTask() {
+		if((Date.now() - time) >= 7000) {
+			lastScrollUpdate = time;
+			time = Date.now();
+			r[next].checked = true;
 
-    doTask();
+			if(reversing == false) {
+				if(next == 4) {
+					next = 3;
+					reversing = true;
+				}
+				else next += 1;
+			}
+			else {
+				if(next == 0) {
+					next = 1;
+					reversing = false;
+				}
+				else next -= 1;
+			}
+		}
+		
+		if((Date.now() - lastScrollUpdate) >= 500) lastScrollUpdate = Date.now()
+		setTimeout(doTask, 500);
+	}
+
+	doTask();
 });
